77af122cbe1e4e99d0d9c118f68e1aae2678e9ce
Pourya-Ardestani <pooryaardestani@gmail.com>
2024-09-28 00:31:24 +0330

    add all endpoints and api keys

--- Day 36/main.py
+++
@@ -1,9 +0,0 @@
-import os
-# sal = os.environ.get("SAL")
-print(os.environ.get('YOUR_VAR'))
-print(os.environ.get('SLA'))
-print(os.environ.get('SAL'))
-print(os.environ.get('dodo'))
-print(os.environ.get('salma'))
-
-
